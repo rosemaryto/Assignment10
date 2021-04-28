cb156fb6c9abe6181f7439a2f6ad590aa77dc13d
rosemaryto <rosemary_to@ymail.com>
2021-04-27 20:04:54 -0700

    parts4

--- beginning/js/deathbyjs.js
+++ beginning/js/deathbyjs.js
@@ -1,9 +1,30 @@
 //STEP 1
+// var word = "webmaster";
+// function sortString() {
+//     var arr = word.split("");
+//     var sorted = arr.sort();
+//     window.console.log(sorted.join());
+// }
+// sortString();
 
 //STEP 2
+const story = "the quick brown fox";
+const words = story.split(" ");
+ 
+for (let i=0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+window.console.log(words.toString());
+
+
 
 //STEP 3
+// var story = "The quick brown fox"
+// var newMessage = story.split(" ");
+// window.console.log(story.match(/(a|e|i|o|u)/g)); 
+
 
 //STEP 4
+
 
 //STEP 5
