41e0e187e7c60757d4a45a067c0ed9768ed50ad7
rosemaryto <rosemary_to@ymail.com>
2021-04-29 22:33:26 -0700

    part4

--- beginning/js/math.js
+++ beginning/js/math.js
@@ -1,9 +1,24 @@
 //STEP 1
+// var urNum = window.prompt("Enter a number");
+// window.console.log(Math.abs(urNum));
 
 //STEP 2
+// var urNum = window.prompt("Enter a decimal number.");
+// window.console.log(Math.ceil(urNum));
 
 //STEP 3
+// var urNum = window.prompt("Enter a decimal number.");
+// window.console.log(Math.floor(urNum));
+
 
 //STEP 4
+var urNum = window.prompt("Enter 5 numbers using commas");
+var nums = [];
+var bigSmlNum = nums.push(`${urNum}`);
+window.console.log(nums);
+
+
 
 //STEP 5
+// var urNum = window.prompt("Enter a decimal number.");
+// window.console.log(Math.sqrt(urNum));
